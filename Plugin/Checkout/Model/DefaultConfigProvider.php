09269d9b753d57cd2e929f140bd515898bb23510
Vijay Rami <vijaymrami@gmail.com>
2022-12-18 20:09:33 +0530

    Solved conflict with Amasty Request a Quote module

--- Plugin/Checkout/Model/DefaultConfigProvider.php
+++ Plugin/Checkout/Model/DefaultConfigProvider.php
@@ -37,26 +37,27 @@ class DefaultConfigProvider
         $items = $result['totalsData']['items'];
         foreach ($items as $index => $item) {
             $quoteItem = $this->checkoutSession->getQuote()->getItemById($item['item_id']);
-            $result['quoteItemData'][$index]['manufacturer'] = $quoteItem->getProduct()->getAttributeText('manufacturer');
-            
-            $product_type = $quoteItem->getProduct()->getTypeId();
-            $sku = $quoteItem->getProduct()->getSku();
-            $qty = $quoteItem->getQty();
-            
-            if($product_type == 'configurable') {
-                $product = $this->productFactory->create();
-                $productdata = $product->loadByAttribute('sku', $sku);
-                $result['quoteItemData'][$index]['current_price_value'] = $productdata->getPriceInfo()->getPrice('final_price')->getAmount()->getValue();
-                $result['quoteItemData'][$index]['regular_price_value'] = $productdata->getPriceInfo()->getPrice('regular_price')->getAmount()->getValue();
-                $originalPrice = $productdata->getPriceInfo()->getPrice('regular_price')->getAmount()->getValue() * $qty;
-                $result['quoteItemData'][$index]['regular_price'] = $this->currencyInterface->format($originalPrice, false, 2);
-            } else {
-                $result['quoteItemData'][$index]['current_price_value'] = $quoteItem->getProduct()->getPriceInfo()->getPrice('final_price')->getAmount()->getValue();
-                $result['quoteItemData'][$index]['regular_price_value'] = $quoteItem->getProduct()->getPriceInfo()->getPrice('regular_price')->getAmount()->getValue();
-                $originalPrice = $quoteItem->getProduct()->getPriceInfo()->getPrice('regular_price')->getAmount()->getValue() * $qty;
-                $result['quoteItemData'][$index]['regular_price'] = $this->currencyInterface->format($originalPrice, false, 2);
-            }
-            
+            if($quoteItem) {
+	            $result['quoteItemData'][$index]['manufacturer'] = $quoteItem->getProduct()->getAttributeText('manufacturer');
+	            
+	            $product_type = $quoteItem->getProduct()->getTypeId();
+	            $sku = $quoteItem->getProduct()->getSku();
+	            $qty = $quoteItem->getQty();
+	            
+	            if($product_type == 'configurable') {
+	                $product = $this->productFactory->create();
+	                $productdata = $product->loadByAttribute('sku', $sku);
+	                $result['quoteItemData'][$index]['current_price_value'] = $productdata->getPriceInfo()->getPrice('final_price')->getAmount()->getValue();
+	                $result['quoteItemData'][$index]['regular_price_value'] = $productdata->getPriceInfo()->getPrice('regular_price')->getAmount()->getValue();
+	                $originalPrice = $productdata->getPriceInfo()->getPrice('regular_price')->getAmount()->getValue() * $qty;
+	                $result['quoteItemData'][$index]['regular_price'] = $this->currencyInterface->format($originalPrice, false, 2);
+	            } else {
+	                $result['quoteItemData'][$index]['current_price_value'] = $quoteItem->getProduct()->getPriceInfo()->getPrice('final_price')->getAmount()->getValue();
+	                $result['quoteItemData'][$index]['regular_price_value'] = $quoteItem->getProduct()->getPriceInfo()->getPrice('regular_price')->getAmount()->getValue();
+	                $originalPrice = $quoteItem->getProduct()->getPriceInfo()->getPrice('regular_price')->getAmount()->getValue() * $qty;
+	                $result['quoteItemData'][$index]['regular_price'] = $this->currencyInterface->format($originalPrice, false, 2);
+	            }
+        	} 
             
         }
         return $result;
